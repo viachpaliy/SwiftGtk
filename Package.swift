58509311d3449017a5392643a3978b75baf6cfd4
viachpaliy <viachpaliy@ukr.net>
2018-06-17 14:35:07 +0300

    change version

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ let package = Package(
     name: "Gtk",
     dependencies: [
         .Package(url: "https://github.com/viachpaliy/CGtk.git", majorVersion: 1),
-        .Package(url: "https://github.com/viachpaliy/SwiftGdk.git", majorVersion: 3),
-        .Package(url: "https://github.com/viachpaliy/SwiftAtk.git", majorVersion: 2)
+        .Package(url: "https://github.com/viachpaliy/SwiftGdk.git",  majorVersion: 1),
+        .Package(url: "https://github.com/viachpaliy/SwiftAtk.git",  majorVersion: 1)
     ],
     swiftLanguageVersions: [3, 4]
 )
