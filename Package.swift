d88fb0f3e02bd70c072b755a8ddc9a155bdfe70e
viachpaliy <viachpaliy@ukr.net>
2018-06-17 10:31:51 +0300

    change repository from original to fork

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@ import PackageDescription
 let package = Package(
     name: "Gtk",
     dependencies: [
-        .Package(url: "https://github.com/rhx/CGtk.git", majorVersion: 1),
-        .Package(url: "https://github.com/rhx/SwiftGdk.git", majorVersion: 3),
-        .Package(url: "https://github.com/rhx/SwiftAtk.git", majorVersion: 2)
+        .Package(url: "https://github.com/viachpaliy/CGtk.git", majorVersion: 1),
+        .Package(url: "https://github.com/viachpaliy/SwiftGdk.git", majorVersion: 3),
+        .Package(url: "https://github.com/viachpaliy/SwiftAtk.git", majorVersion: 2)
     ],
     swiftLanguageVersions: [3, 4]
 )
